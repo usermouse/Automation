dbcc568cb982d05820f150cee35eac8376174e04
usermouse <usermouse@gmail.com>
2018-09-14 16:42:15 +0300

    fix xpath

--- Task/TestTask/WebObject/WriteReviewPopup.cs
+++ Task/TestTask/WebObject/WriteReviewPopup.cs
@@ -5,13 +5,13 @@ namespace TestTask.WebObject
 {
     public class WriteReviewPopup :BasePage
     {
-        public IWebElement CleanSelect => FindElement($"//div[@style='display: block;']//select[@name='reviews_clean']");
-        public IWebElement ComfortSelect => FindElement($"//div[@style='display: block;']//select[@name='reviews_comfort']");
-        public IWebElement LocationSelect => FindElement($"//div[@style='display: block;']//select[@name='reviews_location']");
-        public IWebElement FacilitiesSelect => FindElement($"//div[@style='display: block;']//select[@name='reviews_facilities']");
-        public IWebElement StaffSelect => FindElement($"//div[@style='display: block;']//select[@name='reviews_staff']");
-        public IWebElement ReviewTextArea => FindElement($"//div[@style='display: block;']//textarea[@name='reviews_comments']");
-        public IWebElement CloseButton => FindElement($"//div[@style='display: block;']//button[@type='button' and contains(@class, 'close')]");
-        public IWebElement SubmitButton => FindElement($"//div[@style='display: block;']//button[@type='button' and contains(@class, 'addreview')]");
+        public IWebElement CleanSelect => FindElement($"//div[contains(@style,'display: block;')]//select[@name='reviews_clean']");
+        public IWebElement ComfortSelect => FindElement($"//div[contains(@style,'display: block;')]//select[@name='reviews_comfort']");
+        public IWebElement LocationSelect => FindElement($"//div[contains(@style,'display: block;')]//select[@name='reviews_location']");
+        public IWebElement FacilitiesSelect => FindElement($"//div[contains(@style,'display: block;')]//select[@name='reviews_facilities']");
+        public IWebElement StaffSelect => FindElement($"//div[contains(@style,'display: block;')]//select[@name='reviews_staff']");
+        public IWebElement ReviewTextArea => FindElement($"//div[contains(@style,'display: block;')]//textarea[@name='reviews_comments']");
+        public IWebElement CloseButton => FindElement($"//div[contains(@style,'display: block;')]//button[@type='button' and contains(@class, 'close')]");
+        public IWebElement SubmitButton => FindElement($"//div[contains(@style,'display: block;')]//button[@type='button' and contains(@class, 'addreview')]");
     }
 }
